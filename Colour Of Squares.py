b8d84fe8c1a6c47eea20b3911f3162492515a5e2
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-21 22:24:29 -0400

    Completed Colour of Squares

--- Colour Of Squares.py
+++ Colour Of Squares.py
@@ -2,36 +2,30 @@
 letter, num = list(input(("Input the letter and number: ")))
 num = int(num)
 row = num % 2 
-#For each letter, if statement for black or white
+#For each letter, if statements to determine black or white square
 if letter == "a":
-    if row == 1: print("black")
-    else: 
-        print("white")
+    if row == 1: print("The square is black")
+    else: print("The square is white")
 elif letter == "b":
-    if row == 1: print("white") 
-    else: 
-    print("black")
+    if row == 1: print("The square is white") 
+    else: print("The square is black")
 elif letter == "c":
-    if row == 1: print("black")
-    else: 
-    print("white")
+    if row == 1: print("The square is black")
+    else: print("The square is white")
 elif letter == "d":
-    if row == 1: print("white")
-    else: 
-    print("black")
+    if row == 1: print("The square is white")
+    else: print("The square is black")
 elif letter == "e":
-    if row == 1: print("black")
-    else:
-    print("white")
+    if row == 1: print("The square is black")
+    else: print("The square is white")
 elif letter == "f":
-    if row == 1: print("white")
-    else: 
-    print("black")
+    if row == 1: print("The square is white")
+    else: print("The square is black")
 elif letter == "g":
-    if row == 1: print("black")
-    else: 
-    print("white")
-elif letter == "f":
-    if row == 1: print("white")
-    else: 
-    print("black")
+    if row == 1: print("The square is black")
+    else: print("The square is white")
+elif letter == "h":
+    if row == 1: print("The square is white")
+    else: print("The square is black")
+#Incorrect input by user
+else: print("The input is not valid")
