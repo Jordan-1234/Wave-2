3de4fa3c548626536153fe61747114e763fe39ba
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-21 22:12:25 -0400

    Colour of Squares

--- Colour Of Squares.py
+++ Colour Of Squares.py
@@ -1,29 +1,37 @@
 #User inputs the coordinates
-letter, num = list(input((Input the coordinates)))
+letter, num = list(input(("Input the letter and number: ")))
 num = int(num)
 row = num % 2 
 #For each letter, if statement for black or white
 if letter == "a":
     if row == 1: print("black")
-    else print("white")
+    else: 
+        print("white")
 elif letter == "b":
     if row == 1: print("white") 
-    else print("black")
+    else: 
+    print("black")
 elif letter == "c":
     if row == 1: print("black")
-    else print("white")
+    else: 
+    print("white")
 elif letter == "d":
     if row == 1: print("white")
-    else print("black")
+    else: 
+    print("black")
 elif letter == "e":
     if row == 1: print("black")
-    else print("white")
+    else:
+    print("white")
 elif letter == "f":
     if row == 1: print("white")
-    else print("black")
+    else: 
+    print("black")
 elif letter == "g":
     if row == 1: print("black")
-    else print("white")
+    else: 
+    print("white")
 elif letter == "f":
     if row == 1: print("white")
-    else print("black")
+    else: 
+    print("black")
