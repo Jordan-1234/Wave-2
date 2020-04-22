e1a427c10587b2349cbf9e9e51817b213e9a3bed
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-21 22:26:48 -0400

    fully completed months to days

--- Month to Days.py
+++ Month to Days.py
@@ -2,11 +2,11 @@
 month_name = input("Name of the month: ")
 # Group months with same amount of days
 if month_name == "February":
-    print("Number of days: 28/29 days")
+    print("Number of days: 28/29")
 elif month_name in ("April" , "June" , "September" , "November"):
     print("Number of days: 30")
 elif month_name in ("January" , "March" , "May" , "July", "August" , "October", "December"):
-    print("Number of days: 31 days")
+    print("Number of days: 31")
 # Else statement if month is spelt incorrect or not possible
 else: 
-    print("Incorrect Month Name ") 
+    print("Incorrect month name ") 
