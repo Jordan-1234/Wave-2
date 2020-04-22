fc6ef6e5c97d203a3b2e864bd1a55f87922cb36a
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-21 21:58:57 -0400

    Colour of squares

--- Colour Of Squares.py
+++ Colour Of Squares.py
@@ -5,5 +5,9 @@ row = num % 2
 #For each letter, state black or white
 if letter == "a":
     if row == 1: print("black")
-elif 
-    print(black) 
+    else print("white")
+elif letter == "b":
+    if row == 1: print("white") 
+    else print("black")
+elif letter == "c":
+    if row == 1: print("black")
