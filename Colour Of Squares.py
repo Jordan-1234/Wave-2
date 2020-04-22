a72b06c9fea60de1c00e373b267fa4e03c430b85
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-21 21:30:34 -0400

    Colour of Squares

---
+++ Colour Of Squares.py
@@ -0,0 +1,9 @@
+#User inputs the coordinates
+letter, num = list(input((Input the coordinates)))
+num = int(num)
+row = num % 2 
+#For each letter, state black or white
+if letter == "a":
+    if row == 1: print("black")
+elif 
+    print(black) 
---
+++ Roots of a Quadratic Function.py
@@ -0,0 +1,3 @@
+a = int(input("Input the a value"))
+b = int(input("Input the b value"))
+c = int(input("Input the c value"))
