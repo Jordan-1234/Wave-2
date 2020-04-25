3aa8b0c5c0377bcd3d6f4fcba3d00a6bf744203a
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-25 02:23:51 -0400

    Roots of a Quadratic

---
+++ Roots of Quadratic.py
@@ -0,0 +1,13 @@
+#Import library
+import math
+
+#Inputs for Quadratic Equation
+a = int(input())
+b = int(input())
+c = int(input())
+
+#First, state the discriminant
+if b**2 - 4*a*c < 0:
+    print("There are no real roots")
+else: 
+    
