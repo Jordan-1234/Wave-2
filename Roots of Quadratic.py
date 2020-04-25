3f5e37267f38da06b86cf2badac57247527c013e
Jordan So <jordan.so21@ycdsbk12.ca>
2020-04-25 02:45:55 -0400

    Roots of Quadratic Function

--- Roots of Quadratic.py
+++ Roots of Quadratic.py
@@ -6,8 +6,18 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-#First, state the discriminant
+#First, use the discriminant equation to check for the amount of roots
 if b**2 - 4*a*c < 0:
     print("There are no real roots")
+
+#Use the whole quadratics equation to check for the amount roots
 else: 
-    
+    root1 = int((b*-1 + math.sqrt(b**2 - 4*a*c)) / (2*a))
+    root2 = int((b*-1 + math.sqrt(b**2 - 4*a*c)) / (2*a))
+    if root1 != root2:
+        print("There are 2 real roots")
+        print("Root 1 =", root1)
+        print("Root 2 =", root2)
+    else: 
+        print("There is 1 real root. ")
+        print("Root 1 =", root1)
